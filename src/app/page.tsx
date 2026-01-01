24040d666235067ffd252da53d041d331f54e2d8
boombradford <fluxninelabs@gmail.com>
2026-01-01 09:02:32 -0600

    Add padding and width to main container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ import { BriefingView } from "@/components/briefing-view";
 
 export default function Home() {
   return (
-    <main className="min-h-screen pb-20 pt-8">
-      <div className="max-w-[1600px] mx-auto">
+    <main className="min-h-screen pb-20 pt-8 px-4">
+      <div className="max-w-[1600px] mx-auto w-full">
         <BriefingView />
         <ArticleGrid />
         <FeedManager />
